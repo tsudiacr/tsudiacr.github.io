8c355461b08aa7a73c387aef294f75c26060e975
Diana González <diana.gonzalez@seidor.com>
2019-01-19 23:41:21 -0600

    Primero

---
+++ precache-manifest.063c9b27c4055b2db17da8926c5171fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bbf2a7b6c42d3754592",
+    "url": "/static/css/main.370bf279.chunk.css"
+  },
+  {
+    "revision": "7bbf2a7b6c42d3754592",
+    "url": "/static/js/main.7bbf2a7b.chunk.js"
+  },
+  {
+    "revision": "43a3e7ec585c665e2ef9",
+    "url": "/static/js/1.43a3e7ec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18bbac08b6ed49895a1a7416beca0cdf",
+    "url": "/static/media/flower.18bbac08.png"
+  },
+  {
+    "revision": "07a06542fef2592d07637da0dcf25050",
+    "url": "/static/media/rings.07a06542.png"
+  },
+  {
+    "revision": "e00acf4e09cb535d22e600df7b046b04",
+    "url": "/static/media/fondo.e00acf4e.png"
+  },
+  {
+    "revision": "448c6dfc551b72aa0bb5acbf029b5127",
+    "url": "/index.html"
+  }
+];
